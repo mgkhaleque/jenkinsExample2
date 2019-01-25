b6a22ef56abe34e8d6824a68f0c145334f88962a
ZibonIbrahim <zibonibrahim@yahoo.com>
2019-01-24 21:20:17 -0500

    comments added

--- Amazon/src/main/java/AutomationAmazon/AmazonAutomation.java
+++ Amazon/src/main/java/AutomationAmazon/AmazonAutomation.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.util.concurrent.TimeUnit;
 //author zibonIbrahim!
+// Sloooooow to respond!!
 public class AmazonAutomation {
     public static WebDriver driver = null;
     public String baseUrl;
