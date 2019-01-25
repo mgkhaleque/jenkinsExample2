f1bd24d23fc37bc597e71affcbe66a2725935390
ZibonIbrahim <zibonibrahim@yahoo.com>
2019-01-24 21:16:14 -0500

    small change added

--- Amazon/src/main/java/AutomationAmazon/AmazonAutomation.java
+++ Amazon/src/main/java/AutomationAmazon/AmazonAutomation.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.util.concurrent.TimeUnit;
+//author zibonIbrahim!
 public class AmazonAutomation {
     public static WebDriver driver = null;
     public String baseUrl;
